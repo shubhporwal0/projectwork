b674b09c281d7cb5cb1dd5dae523a08e6e95db88
shubhporwal0 <66530626+shubhporwal0@users.noreply.github.com>
2024-09-28 19:43:20 +0200

    Update models.py
    line 9, post_date to posting_date

--- jobs/models.py
+++ jobs/models.py
@@ -6,7 +6,7 @@ class Job(models.Model):
     title = models.CharField(max_length=255, db_column='Title')  # Map to 'Title' column
     #department = models.CharField(max_length=255, db_column='Department', null=True, blank=True)  # Map to 'Department' and handle null
     contract_location = models.CharField(max_length=255, db_column='contract_location')  # Map to 'contract_location'
-    posting_date = models.DateField(db_column='Post_date')  # Map to 'Post_date'
+    posting_date = models.DateField(db_column='posting_date')  # Map to 'Post_date'
     employment_type = models.CharField(max_length=255, db_column='Employment_type')  # Map to 'Employment_type'
     application_link = models.URLField(max_length=200, db_column='application_link')  # Map to 'application_link'
     long_description = models.TextField(db_column='long_description')  # Map to 'long_description'
